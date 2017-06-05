cd65df4c2e26aba57712e036437868f0974ea225
Dorian Bramarov <dbramarov@yahoo.com>
2017-06-05 11:40:24 -0400

    Updated Portfolio page

--- Rails/portfolioII/app/assets/javascripts/js/init.js
+++ Rails/portfolioII/app/assets/javascripts/js/init.js
@@ -1,6 +1,10 @@
 (function($){
   $(function(){
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+    	menuWidth: 180, // Default is 300
+      	closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      	draggable: true // Choose whether you can drag to open on touch screens
+    });
     $('.modal').modal();
     $('.parallax').parallax();
   }); // end of document ready
