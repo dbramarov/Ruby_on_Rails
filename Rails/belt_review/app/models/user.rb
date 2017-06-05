57bb7bc1ae3446327b1f674e66fea4035ef541aa
Dorian Bramarov <dbramarov@yahoo.com>
2017-06-04 20:02:58 -0400

    Updated Ruby Files

--- Rails/belt_review/app/models/user.rb
+++ Rails/belt_review/app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  PASSWORD_VALIDATOR = /\A(?=.*\d)[A-Za-z\d]{8,}\z/i
   validates :email, uniqueness: { case_sensitive:false }, format: { with: EMAIL_REGEX}
-  validates :password, :password_confirmation, presence: true, length: {minimum: 8},  :on => :create
+  validates :password, :password_confirmation, presence: true, length: {minimum: 8}, format: {with: PASSWORD_VALIDATOR, message: "must be at least 8 characters and include one number"} , :on => :create
   validates :first_name, :last_name, :email, :city, :state, presence: true
 
   has_many :events
---
+++ Rails/portfolioII/app/assets/javascripts/js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.modal').modal();
+    $('.parallax').parallax();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
